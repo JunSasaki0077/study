8c46f1bd1f2c9db273f652d50fe68606a987a972
Junsasaki0077 <ikasas0077@gmail.com>
2023-05-06 19:47:51 +0900

    lesson18 ReactでAPIを叩く

--- qin/react/src/pages/index.jsx
+++ qin/react/src/pages/index.jsx
@@ -1,10 +1,20 @@
 import Head from "next/head";
-import styles from "@/styles/Home.module.css";
 import { useCallback, useEffect, useState } from "react";
 
-import { Posts } from "@/components/Posts";
-
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = useCallback(async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const json = await res.json();
+    setPosts(json);
+  }, []);
+
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+
+  console.log(posts);
   return (
     <div>
       <Head>
@@ -13,7 +23,13 @@ const Home = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Posts />
+      {posts.length > 0 ? (
+        <ol>
+          {posts.map((post) => {
+            return <li key={post.id}>{post.title}</li>;
+          })}
+        </ol>
+      ) : null}
     </div>
   );
 };
