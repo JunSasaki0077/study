23cd63789b53a72706bd3e83ba8c814aad1ae33b
Jun Sasaki <71161710+JunSasaki0077@users.noreply.github.com>
2023-05-29 11:57:39 +0900

    💄 hooksコメント、ユーザー追加

---
+++ qin/react/src/hooks/useComment.jsx
@@ -0,0 +1,29 @@
+import { fetcher } from "@/utils/fetcher";
+import { useRouter } from "next/router";
+
+import useSWR from "swr";
+
+export const useComment = () => {
+  const router = useRouter();
+
+  const { data: comment, error: commentError } = useSWR(
+    router.query.id
+      ? `https://jsonplaceholder.typicode.com/posts/${router.query.id}`
+      : null,
+    fetcher
+  );
+
+  const { data: user, error: userError } = useSWR(
+    comment?.userId
+      ? `https://jsonplaceholder.typicode.com/users/${comment.userId}`
+      : null,
+    fetcher
+  );
+
+  return {
+    comment,
+    user,
+    error: commentError || userError,
+    isLoading: !user && !userError,
+  };
+};
