10ab62cf2d36a1b1ba97c6f9c644419f44c7564f
Junsasaki0077 <ikasas0077@gmail.com>
2023-06-02 08:08:48 +0900

    ユーザーページに投稿一覧追加

--- qin/react/src/components/User/index.jsx
+++ qin/react/src/components/User/index.jsx
@@ -1,5 +1,6 @@
 import { useUser } from "@/hooks/useUser";
 import Head from "next/head";
+import { PostsByUserId } from "../Posts/PostsByUserId";
 
 const User = () => {
   const { data, error, isLoading } = useUser();
@@ -18,9 +19,13 @@ const User = () => {
         <title>{data?.name}</title>
       </Head>
       <h1>{data.name}</h1>
+      <h2>詳細</h2>
       <p>{data.email}</p>
       <p>{data.address.street}</p>
       <p>{data.address.suite}</p>
+      <h2>投稿</h2>
+      <PostsByUserId id={data.id} />
+      <h2>コメント</h2>
     </div>
   );
 };
