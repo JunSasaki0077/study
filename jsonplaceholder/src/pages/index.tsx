0cd989e31366bd0f9b81022a4eea801258f9cef2
Jun Sasaki <71161710+JunSasaki0077@users.noreply.github.com>
2023-06-16 13:33:00 +0900

    Add files via upload

---
+++ jsonplaceholder/src/pages/index.tsx
@@ -0,0 +1,39 @@
+import { fetchPostData } from "@/lib/api";
+import Link from "next/link";
+
+type Post = {
+  id: number;
+  title: string;
+};
+
+type HomeProps = {
+  posts: Post[];
+};
+
+const Home: React.FC<HomeProps> = ({ posts }) => {
+  return (
+    <div>
+      <ul>
+        {posts.map((post: any) => (
+          <li key={post.id}>
+            <Link href={`/${post.id}`}>
+              <h2>{post.title}</h2>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
+
+export const getStaticProps = async () => {
+  const posts = await fetchPostData();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
