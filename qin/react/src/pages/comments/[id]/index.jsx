a1d0c1c74b186f43e0d5bcc9a7875e2343aa8f91
Junsasaki0077 <ikasas0077@gmail.com>
2023-07-21 21:02:54 +0900

    lesson33 個別ページSG化

--- qin/react/src/pages/comments/[id]/index.jsx
+++ qin/react/src/pages/comments/[id]/index.jsx
@@ -1,12 +1,44 @@
 import { Comment } from "@/components/Comment";
 import { Header } from "@/components/header";
 import React from "react";
+import { SWRConfig } from "swr";
 
-const CommentId = () => {
+export const getStaticPaths = async () => {
+  const comments = await fetch("https://jsonplaceholder.typicode.com/comments");
+  const commentsData = await comments.json();
+  const paths = commentsData.map((comment) => ({
+    params: { id: comment.id.toString() },
+  }));
+
+  return {
+    paths: paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (ctx) => {
+  const { id } = ctx.params;
+  const COMMENT_API_URL = `https://jsonplaceholder.typicode.com/comments/${id}`;
+  const comment = await fetch(COMMENT_API_URL);
+  const commentData = await comment.json();
+
+  return {
+    props: {
+      fallback: {
+        [COMMENT_API_URL]: commentData,
+      },
+    },
+  };
+};
+
+const CommentId = (props) => {
+  const { fallback } = props;
   return (
     <div>
       <Header />
-      <Comment />
+      <SWRConfig value={{ fallback }}>
+        <Comment />
+      </SWRConfig>
     </div>
   );
 };
