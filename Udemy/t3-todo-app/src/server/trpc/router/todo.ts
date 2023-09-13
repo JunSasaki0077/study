9362cfd063ec1c52e2ccc3d32e272ef3c2e2712a
Junsasaki0077 <ikasas0077@gmail.com>
2023-09-14 08:31:16 +0900

    🎨 getSingleTask,deleteTask作成

--- Udemy/t3-todo-app/src/server/trpc/router/todo.ts
+++ Udemy/t3-todo-app/src/server/trpc/router/todo.ts
@@ -32,4 +32,38 @@ export const todoRouter = t.router({
       },
     });
   }),
+  getSingleTask: authedProcedure
+    .input(getSingleTaskSchema)
+    .query(({ ctx, input }) => {
+      return ctx.prisma.task.findUnique({
+        where: {
+          id: input.taskId,
+        },
+      });
+    }),
+
+  updateTask: authedProcedure
+    .input(updateTaskSchema)
+    .mutation(async ({ ctx, input }) => {
+      const task = await ctx.prisma.task.update({
+        where: {
+          id: input.taskId,
+        },
+        data: {
+          title: input.title,
+          body: input.body,
+        },
+      });
+      return task;
+    }),
+
+  deleteTask: authedProcedure
+    .input(deleteTaskSchema)
+    .mutation(async ({ ctx, input }) => {
+      await ctx.prisma.task.delete({
+        where: {
+          id: input.taskId,
+        },
+      });
+    }),
 });
