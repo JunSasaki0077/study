6b275f68df24b13809b115afb965a2cfde7ba451
Junsasaki0077 <ikasas0077@gmail.com>
2023-09-26 22:53:12 +0900

    🔥 不要なコードの削除

--- qin/state/Recoil01/src/components/TodoCounter.tsx
+++ qin/state/Recoil01/src/components/TodoCounter.tsx
@@ -2,11 +2,7 @@ import { FC } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { todosLengthState, todosState } from "src/state/todo";
 
-type Props = {
-  todoCount: number;
-};
-
-export const TodoCounter: FC<Props> = ({ todoCount }) => {
+export const TodoCounter: FC = () => {
   const todosLength = useRecoilValue(todosLengthState);
   return <h2>TODO: {todosLength}件</h2>;
 };
