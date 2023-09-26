cd3adda261df5d8ace32507487c0ce4e3f453bb1
Junsasaki0077 <ikasas0077@gmail.com>
2023-09-26 22:43:41 +0900

    ✨ RecoilでTodoアプリ作成

---
+++ qin/state/Recoil01/src/pages/_app.tsx
@@ -0,0 +1,32 @@
+import type { AppProps } from "next/app";
+import { useState } from "react";
+import { RecoilRoot } from "recoil";
+
+import { Layout } from "src/components/Layout";
+import { Todo } from "src/types";
+
+const TODOS: Todo[] = [
+  {
+    id: 1,
+    text: "foo",
+    isDone: false,
+  },
+  {
+    id: 2,
+    text: "bar",
+    isDone: true,
+  },
+];
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const [todos, setTodos] = useState<Todo[]>(TODOS);
+  return (
+    <>
+      <RecoilRoot>
+        <Layout todoCount={todos.length}>
+          <Component {...pageProps} todos={todos} setTodos={setTodos} />
+        </Layout>
+      </RecoilRoot>
+    </>
+  );
+}
