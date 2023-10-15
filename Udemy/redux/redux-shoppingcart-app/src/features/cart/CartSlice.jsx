64098fae1d6ee03594e2e0a0331dedc8a5b86154
Jun Sasaki <ikasas0077@gmail.com>
2023-10-15 18:08:51 +0900

    ⚡️ 全削除ボタンで中身を削除

--- Udemy/redux/redux-shoppingcart-app/src/features/cart/CartSlice.jsx
+++ Udemy/redux/redux-shoppingcart-app/src/features/cart/CartSlice.jsx
@@ -1,8 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
+import cartItems from "../../cartItems";
 
 //買い物かごの初期化
 const initialState = {
-  cartItems: [],
+  cartItems: cartItems,
   amount: 4,
   total: 0,
 };
@@ -10,7 +11,12 @@ const initialState = {
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    clearCart: (state) => {
+      return { cartItems: [], amount: 0, total: 0 };
+    },
+  },
 });
 
+export const { clearCart } = cartSlice.actions;
 export default cartSlice.reducer;
