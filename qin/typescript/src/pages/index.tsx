0bdbd63e9193f71607047088310f58acd071a918
Jun Sasaki <ikasas0077@gmail.com>
2023-10-28 13:44:23 +0900

    lesson7 オブジェクトの型

--- qin/typescript/src/pages/index.tsx
+++ qin/typescript/src/pages/index.tsx
@@ -1,17 +1,21 @@
 import { NextPage } from "next";
 
-const foo = (bar: "a" | "b") => {
-  switch (bar) {
-    case "a":
-      return;
-    case "b":
-      return;
-    default:
-      bar;
-      break;
-  }
+//型の引数の第一引数は左側のこと
+//第二引数は右側を指定する
+let obj3: Record<string, unknown> = {
+  a: 1,
+  b: "foo",
+};
+let obj4: { a: number; b: string; foo: string } = {
+  a: 1,
+  b: "foo",
+  foo: "foo",
 };
 
-export const Home: NextPage = () => {
+obj3.foo = "bar";
+
+const Home: NextPage = () => {
   return <div>test</div>;
 };
+
+export default Home;
