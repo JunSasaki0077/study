857a083474113b4b3c273567a0c57d7f19ddfa2b
Jun Sasaki <ikasas0077@gmail.com>
2023-10-30 08:19:24 +0900

    lesson8 Intersection Types Union Types

--- qin/typescript/src/pages/index.tsx
+++ qin/typescript/src/pages/index.tsx
@@ -1,18 +1,10 @@
 import { NextPage } from "next";
 
-//型の引数の第一引数は左側のこと
-//第二引数は右側を指定する
-let obj3: Record<string, unknown> = {
-  a: 1,
-  b: "foo",
-};
-let obj4: { a: number; b: string; foo: string } = {
-  a: 1,
-  b: "foo",
-  foo: "foo",
-};
+type Foo = number;
 
-obj3.foo = "bar";
+type Bar = string;
+
+type FooBar = Foo | Bar;
 
 const Home: NextPage = () => {
   return <div>test</div>;
