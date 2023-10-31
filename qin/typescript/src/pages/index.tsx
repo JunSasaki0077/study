6aa4c12d18d0f9a0ad7e23d25205696be1f8bdbd
Jun Sasaki <ikasas0077@gmail.com>
2023-11-01 08:28:10 +0900

    lesson10Reactコンポーネントに型をつけてみる１

--- qin/typescript/src/pages/index.tsx
+++ qin/typescript/src/pages/index.tsx
@@ -1,18 +1,22 @@
+import { TwitterCard } from "@/components/TwitterCard";
 import { NextPage } from "next";
 
-type Animals = "dog" | "cat";
-
-type Foo = {
-  [key in Animals]: number;
-};
-
-const foo: Foo = {
-  dog: 2,
-  cat: 2,
-};
-
 const Home: NextPage = () => {
-  return <div>test</div>;
+  return (
+    <TwitterCard
+      user={{
+        name: "しまぶー",
+        accountName: "shimabu_it",
+        image:
+          "https://doodleipsum.com/200x200/avatar-5?bg=6392D9&i=0f33dd99912f70859db2eeaf52dd71f4",
+      }}
+      body={{
+        text: "おはようございます！！！",
+        image:
+          "https://doodleipsum.com/700x394/flat?bg=7463D9&i=c2a3b28fed2bb5d5690e9bab3ff2dce1",
+      }}
+    />
+  );
 };
 
 export default Home;
