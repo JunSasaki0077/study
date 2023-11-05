0613d80d576c0237c86e4acd2925d385aca04bb8
Jun Sasaki <ikasas0077@gmail.com>
2023-11-05 10:41:04 +0900

    lesson 14 ダウンキャストアップキャスト

--- qin/typescript/index.ts
+++ qin/typescript/index.ts
@@ -1,21 +1,10 @@
-export function double(x: number | string) {
-  if (typeof x === "string") {
-    return Number(x) * 2;
-  }
-  return x * 2;
+export const PATH = {
+  INDEX: "/",
+  LOGIN: "/login",
+  REGISTER: "/register",
+  PROFILE: "/profile",
+} as const;
+
+export function getFirstLetter(str: number) {
+  return (str as unknown as string).charAt(0);
 }
-
-export const Obj = {
-  foo: "foo",
-  bar: "bar",
-};
-
-type Key = keyof typeof Obj;
-
-const key: Key = "foo";
-
-function test(x: keyof typeof Obj) {
-  return;
-}
-
-test("foo");
