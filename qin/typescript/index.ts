95b90375695deb12ccb4702530396168470e2069
Jun Sasaki <ikasas0077@gmail.com>
2023-11-06 07:59:26 +0900

    ユーザー定義の型ガード

--- qin/typescript/index.ts
+++ qin/typescript/index.ts
@@ -1,21 +1,37 @@
 type UserA = { name: string; lang: "ja" };
 type UserB = { name: string; lang: "en" };
-type UserC = { name: string; lang: "fr" };
 
-export const foo = (value: UserA | UserB | UserC) => {
-  switch (value.lang) {
-    case "ja": {
-      return value;
-    }
-    case "en": {
-      return value;
-    }
-    case "fr": {
-      return value;
-    }
-    default: {
-      throw Error("lang is node defined!");
-      return value;
-    }
+const isUserA = (user: UserA | UserB): user is UserA => {
+  return user.lang === "ja";
+};
+const isUserB = (user: UserA | UserB): user is UserB => {
+  return user.lang === "en";
+};
+
+export const foo = (value: any) => {
+  if (isUserA(value)) {
+    return value;
+  }
+  if (isUserB(value)) {
+    return value;
+  }
+  return value;
+};
+
+//非同期の型定義
+export const bar = async () => {
+  const res = await fetch("");
+  const json = await res.json();
+  if (isUserA(json)) {
+    return json.lang;
   }
 };
+
+//filter関数の型定義
+const users: (UserA | UserB)[] = [
+  { name: "たなか", lang: "ja" },
+  { name: "やまだ", lang: "ja" },
+  { name: "ジョニー", lang: "en" },
+];
+
+const japanese = users.filter(isUserA);
