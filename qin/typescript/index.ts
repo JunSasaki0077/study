06895cce23be299bbf8f5f1cf5e5aaccc8533faa
Jun Sasaki <ikasas0077@gmail.com>
2023-11-08 18:28:25 +0900

    lesson21 Conditional Types

--- qin/typescript/index.ts
+++ qin/typescript/index.ts
@@ -1,19 +1,23 @@
-const getProperty = <T, K extends keyof T>(obj: T, key: K) => {
-  return obj[key];
+type Props = {
+  id: string;
+  name: string;
+  age: number;
 };
 
-const setProperty = <T, K extends keyof T>(obj: T, key: K, value: T[K]) => {
-  obj[key] = value;
+type Filter<T, U> = {
+  [K in keyof T]: T[K] extends U ? K : never;
+}[keyof T];
+
+type StringKeys = Filter<Props, string>;
+type NumberKeys = Filter<Props, number>;
+type BooleanKeys = Filter<Props, boolean>;
+
+const foo = (id: string, age: number) => {
+  return 1;
 };
 
-const obj = {
-  foo: 1,
-  bar: 2,
-  baz: "3",
-};
+type Return<T> = T extends (...args: [any, infer U, ...any[]]) => any
+  ? U
+  : never;
 
-const hoge = getProperty(obj, "baz");
-
-setProperty(obj, "bar", 3);
-
-const foo = [1, 2, 3].map((v) => v.toString());
+type Foo = Return<typeof foo>;
