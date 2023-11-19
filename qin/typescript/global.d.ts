0211b9b081af6c04c149487283a381aa5ee3d697
Jun Sasaki <ikasas0077@gmail.com>
2023-11-20 08:12:11 +0900

    lesson25 モジュール拡張　アンビエントモジュール

--- qin/typescript/global.d.ts
+++ qin/typescript/global.d.ts
@@ -1,5 +1,7 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    readonly FOO: string;
-  }
+// import { FC } from "react";
+// declare module "react" {
+//   type CFC<P = {}> = FC<P & { className?: string }>;
+// }
+declare module "*.mp3" {
+  export const data: string;
 }
---
+++ qin/typescript/index.tsx
@@ -0,0 +1,6 @@
+// import { CFC, FC } from "react";
+import data from "hogehoge.mp3";
+
+// const Componet: CFC = (props) => {
+//   return <div className={props.className}>text</div>;
+// };
