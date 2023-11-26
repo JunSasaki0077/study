a057021756d33e8c356167e0f4b22d8d04e363cc
Jun Sasaki <ikasas0077@gmail.com>
2023-11-26 14:15:01 +0900

    補習　Template Literal Typesとsatifies演算子

--- qin/typescript/index.tsx
+++ qin/typescript/index.tsx
@@ -1,6 +1,37 @@
-// import { CFC, FC } from "react";
-import data from "hogehoge.mp3";
+type Hex = `#${string}`;
 
-// const Componet: CFC = (props) => {
-//   return <div className={props.className}>text</div>;
-// };
+const red: Hex = "#ff0000";
+
+type AppVersion = `v${number}.${number}.${number}`;
+const appVersion: AppVersion = "v1.2.0";
+
+type Rgb = [number, number, number];
+
+type Colors = {
+  [color in "red" | "blue" | "green"]: Hex | Rgb;
+};
+
+const foo = async () => {
+  const res = await fetch("");
+  const json = await res.json();
+};
+
+const colors = {
+  red: "#ff0000",
+  blue: "#0000ff",
+  green: [0, 255, 0],
+  //   yellow: "#ffff00",
+} as const satisfies Colors;
+
+colors.green = [1, 2, 3];
+
+colors.green.map((v) => v);
+
+type Path = {
+  [key: string]: `/${string}`;
+};
+
+const path = {
+  index: "/",
+  about: "/about",
+} as const satisfies Path;
