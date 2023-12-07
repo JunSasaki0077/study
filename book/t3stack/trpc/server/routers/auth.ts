211359a715e61158b78c326182e0692a599a6a10
Jun Sasaki <ikasas0077@gmail.com>
2023-12-07 22:25:40 +0900

    サインアップ用の API作成

--- book/t3stack/trpc/server/routers/auth.ts
+++ book/t3stack/trpc/server/routers/auth.ts
@@ -8,6 +8,58 @@ import crypto from "crypto";
 const ONE_SECOND = 1000;
 const ONE_MINUTE = ONE_SECOND * 60;
 const ONE_HOUR = ONE_MINUTE * 60;
-const ONEDAY = ONE_HOUR * 24;
+const ONE_DAY = ONE_HOUR * 24;
 
-export const authRouter = router({});
+export const authRouter = router({
+  singUp: publicProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string(),
+      })
+    )
+    .mutation(async ({ input }) => {
+      try {
+        const { name, email, password } = input;
+
+        // メールアドレスの重複チェック
+        const user = await prisma.user.findUnique({
+          where: { email },
+        });
+
+        if (user) {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: "既に登録されているメールアドレスです",
+          });
+        }
+
+        // パスワードのハッシュ化
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        // ユーザーの作成
+        await prisma.user.create({
+          data: {
+            email,
+            name,
+            hashedPassword,
+          },
+        });
+      } catch (error) {
+        console.log(error);
+
+        if (error instanceof TRPCError && error.code === "BAD_REQUEST") {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: error.message,
+          });
+        } else {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "エラーが発生しました",
+          });
+        }
+      }
+    }),
+});
