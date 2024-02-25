1b7848cf7a8a6a681657b76d33c4d5de63a0db61
Jun Sasaki <ikasas0077@gmail.com>
2024-02-26 00:00:37 +0900

    variants使用

--- react/framer-motion-demo/src/App.tsx
+++ react/framer-motion-demo/src/App.tsx
@@ -2,11 +2,24 @@ import { motion } from "framer-motion";
 import "./App.css";
 
 function App() {
+  const variants = {
+    animate: {
+      x: [-100, 200, -500],
+      transition: {
+        duration: 2,
+        times: [0, 0.75, 1],
+        repeat: 2,
+        repeatDelay: 2,
+      },
+    },
+  };
+
   return (
     <>
       <div className="flex justify-center items-center">
         <motion.div
-          whileHover={{ opacity: 0 }}
+          variants={variants}
+          animate="animate"
           className="w-[170px] h-[170px] rounded-full bg-sky-500"
         ></motion.div>
       </div>
