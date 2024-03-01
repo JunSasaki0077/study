6f4f0c4eb324fc48b4fbf6b40d3330dfaf6c1af3
Jun Sasaki <ikasas0077@gmail.com>
2024-03-01 23:13:45 +0900

    prisma instance作成

--- nexttjs/bbs-with-nextjs/app/page.tsx
+++ nexttjs/bbs-with-nextjs/app/page.tsx
@@ -1,4 +1,8 @@
+import { PrismaClient } from "@prisma/client";
 import BBSCardList from "./components/BBSCardList";
+import prisma from "../lib/prismaClient";
+
+const prisma = new PrismaClient();
 
 export default function Home() {
   return (
