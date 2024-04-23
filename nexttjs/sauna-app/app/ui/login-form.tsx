184b225d971af8b683c7cba36ef84514a552fbab
Jun Sasaki <ikasas0077@gmail.com>
2024-04-23 23:22:34 +0900

    ✨ログイン機能実装

--- nexttjs/sauna-app/app/ui/login-form.tsx
+++ nexttjs/sauna-app/app/ui/login-form.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   AtSymbolIcon,
   KeyIcon,
@@ -5,10 +7,14 @@ import {
 } from "@heroicons/react/24/outline";
 import { ArrowRightIcon } from "@heroicons/react/20/solid";
 import Button from "./button";
+import { useFormState, useFormStatus } from "react-dom";
+import { authenticate } from "../lib/actions";
 
 export default function LoginForm() {
+  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
+
   return (
-    <form className="space-y-3">
+    <form action={dispatch} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className="mb-3 text-2xl text-gray-500">ログイン</h1>
         <div className="w-full mb-5">
@@ -53,15 +59,27 @@ export default function LoginForm() {
           </div>
         </div>
         <LoginButton />
-        <div className="flex h-8 items-end space-x-1"></div>
+        <div
+          className="flex h-8 items-end space-x-1"
+          aria-live="polite"
+          aria-atomic="true"
+        >
+          {errorMessage && (
+            <>
+              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+              <p className="text-sm text-red-500">{errorMessage}</p>
+            </>
+          )}
+        </div>
       </div>
     </form>
   );
 }
 
 function LoginButton() {
+  const { pending } = useFormStatus();
   return (
-    <Button appearance="primary" size="xl">
+    <Button appearance="primary" size="xl" aria-disabled={pending}>
       ログイン <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
     </Button>
   );
