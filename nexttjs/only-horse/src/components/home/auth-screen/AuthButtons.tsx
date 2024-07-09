bae6cb058f40b5bc56d1fd9c6ffb4ffa41f8228f
Jun Sasaki <ikasas0077@gmail.com>
2024-07-09 14:11:43 +0900

    Kinde Auth追加

--- nexttjs/only-horse/src/components/home/auth-screen/AuthButtons.tsx
+++ nexttjs/only-horse/src/components/home/auth-screen/AuthButtons.tsx
@@ -1,12 +1,20 @@
 import { Button } from "@/components/ui/button";
+import {
+  LoginLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
 
 const AuthButtons = () => {
   return (
     <div className="flex gap-3 flex-1 md:flex-row flex-col">
-      <Button className="w-full" variant={"outline"}>
-        Sign up
-      </Button>
-      <Button className="w-full">Login</Button>
+      <RegisterLink className="flex-1">
+        <Button className="w-full" variant={"outline"}>
+          Sign up
+        </Button>
+      </RegisterLink>
+      <LoginLink className="flex-1">
+        <Button className="w-full">Login</Button>
+      </LoginLink>
     </div>
   );
 };
