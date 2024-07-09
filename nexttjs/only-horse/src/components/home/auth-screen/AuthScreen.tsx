21939dd0fe6593b9b28d84ed744c707b96bd0ab9
Jun Sasaki <ikasas0077@gmail.com>
2024-07-10 00:57:45 +0900

    Video追加

--- nexttjs/only-horse/src/components/home/auth-screen/AuthScreen.tsx
+++ nexttjs/only-horse/src/components/home/auth-screen/AuthScreen.tsx
@@ -1,10 +1,27 @@
 import { ModeToggle } from "@/components/ModeToggle";
 import HeroSection from "./HeroSection";
+import UnderlinedText from "./UnderlinedText";
+import TodayHighlight from "./TodayHighlight";
 
 const AuthScreen = () => {
   return (
     <div className="flex flex-col">
       <HeroSection />
+
+      <div className="mb-20 mt-12">
+        <div className="max-w-6xl mx-auto px-4">
+          <p className="text-3xl md:text-5xl tracking-tight mt-4 mb-8 font-semibold text-center">
+            Today's
+            <UnderlinedText className="underline-offset-8 md:underline-offset-[12px] decoration-wavy">
+              Hightlight
+            </UnderlinedText>
+            <span className="text-2xl md:text-4xl ml-1">👇</span>
+          </p>
+          <div className="flex flex-col gap-10 mt-10">
+            <TodayHighlight />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
