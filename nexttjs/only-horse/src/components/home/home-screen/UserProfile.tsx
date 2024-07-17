96dba4eb03af9d224db447bd84f71183cd95a4da
Jun Sasaki <ikasas0077@gmail.com>
2024-07-17 19:00:24 +0900

    UserProfileコンポーネント作成

--- nexttjs/only-horse/src/components/home/home-screen/UserProfile.tsx
+++ nexttjs/only-horse/src/components/home/home-screen/UserProfile.tsx
@@ -1,10 +1,48 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import CoverImage from "./CoverImage";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const UserProfile = () => {
+  const isSubscribeb = false;
   return (
     <div className="flex flex-col">
       <CoverImage />
-      <div className="flex flex-col p-4"></div>
+      <div className="flex flex-col p-4">
+        <div className="flex flex-col md:flex-row gap-4 justify-between">
+          <Avatar className="w-20 h-20 border-2 -mt-10">
+            <AvatarImage src={"/user-placeholder.png"} />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <div className="flex">
+            {!isSubscribeb && (
+              <Button asChild className="rounded-full flex gap-10">
+                <Link href={"/pricing"}>
+                  <span className="uppercase font-semibold tracking-wide">
+                    Subscribe
+                  </span>
+                </Link>
+              </Button>
+            )}
+            {isSubscribeb && (
+              <Button className="rounded-full flex gap-10" variant={"outline"}>
+                <span className="uppercase font-semibold tracking-wide">
+                  Subscribed
+                </span>
+              </Button>
+            )}
+          </div>
+        </div>
+        <div className="flex flex-col mt-4">
+          <p className="text-lg font-semibold">John Doe</p>
+          <p className="text-sm mt-2 md:text-md">
+            Discover daily tips and tricks for horse health and care, along with
+            insights into my personal routine with my horses. Subscribe now to
+            gain access to exclusive content and become part of the community.
+          </p>
+        </div>
+      </div>
+      <div aria-hidden="true" className="h-2 w-full bg-muted" />
     </div>
   );
 };
