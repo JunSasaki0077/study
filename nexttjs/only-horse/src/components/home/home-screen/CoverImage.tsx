6e4bd7bcf717deb62855d065f26d0bb5a99b14f9
Jun Sasaki <ikasas0077@gmail.com>
2024-08-06 19:40:54 +0900

    Homeプロフィール上部、画像、ビデオ、Likes動的表示

--- nexttjs/only-horse/src/components/home/home-screen/CoverImage.tsx
+++ nexttjs/only-horse/src/components/home/home-screen/CoverImage.tsx
@@ -1,7 +1,35 @@
+import prisma from "@/db/prisma";
 import { Heart, Image as ImageIcon, Video } from "lucide-react";
 import Image from "next/image";
 
-const CoverImage = () => {
+const CoverImage = async () => {
+  const imageCount = await prisma.post.count({
+    where: {
+      mediaType: "image",
+    },
+  });
+  const videoCount = await prisma.post.count({
+    where: {
+      mediaType: "video",
+    },
+  });
+
+  const totalLikes = await prisma.post.aggregate({
+    _sum: {
+      likes: true,
+    },
+  });
+
+  function formatNumber(num: number) {
+    if (num >= 1000000) {
+      return (num / 1000000).toFixed(1).replace(/\.0$/, "") + "M";
+    }
+    if (num >= 1000) {
+      return (num / 1000).toFixed(1).replace(/\.0$/, "") + "K";
+    }
+    return num.toString();
+  }
+
   return (
     <div className="h-44 overflow-hidden relative">
       <Image
@@ -21,17 +49,19 @@ const CoverImage = () => {
             <div className="flex gap-2 items-center">
               <div className="flex items-center gap-1">
                 <ImageIcon className="w-4 h-4" />
-                <span className="text-sm font-bold">45</span>
+                <span className="text-sm font-bold">{imageCount}</span>
               </div>
               <span className="text-xs">・</span>
               <div className="flex items-center gap-1">
                 <Video className="w-4 h-4" />
-                <span className="text-sm font-bold">67</span>
+                <span className="text-sm font-bold">{videoCount}</span>
               </div>
               <span className="text-xs">・</span>
               <div className="flex items-center gap-1">
                 <Heart className="w-4 h-4" />
-                <span className="text-sm font-bold">10K</span>
+                <span className="text-sm font-bold">
+                  {formatNumber(totalLikes._sum.likes || 0)}
+                </span>
               </div>
             </div>
           </div>
