bfb66d9be00929c6e59c47de9625bc85884d616d
Jun Sasaki <ikasas0077@gmail.com>
2024-08-20 15:02:14 +0900

    右サイドバー作成

--- nexttjs/jsm_banking/app/(root)/page.tsx
+++ nexttjs/jsm_banking/app/(root)/page.tsx
@@ -1,8 +1,13 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  const loggedIn = { firstName: "Adrian" };
+  const loggedIn = {
+    firstName: "Jun",
+    lastName: "Sasaki",
+    email: "example@gmail.com",
+  };
 
   return (
     <section className="home">
@@ -20,7 +25,13 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transaction={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500.5 }]}
+      />
     </section>
   );
 };
