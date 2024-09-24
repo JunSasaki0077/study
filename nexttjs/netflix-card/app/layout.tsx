7f97e5f3785471513de11229af22cc7a4a3dcc95
Jun Sasaki <ikasas0077@gmail.com>
2024-09-24 14:55:21 +0900

    ログイン、サインアップ機能追加

--- nexttjs/netflix-card/app/layout.tsx
+++ nexttjs/netflix-card/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { NextAuthProvider } from "./components/NextAuthProviders";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,11 +25,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <NextAuthProvider>{children}</NextAuthProvider>
       </body>
     </html>
   );
