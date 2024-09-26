328ac060d3e524df094386b029d01c861f33b31b
Jun Sasaki <ikasas0077@gmail.com>
2024-09-26 15:18:03 +0900

    最近追加されたもの画像追加

---
+++ nexttjs/netflix-card/app/components/RecentlyAdded.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import prisma from "../utils/db";
+
+const getData = async () => {
+  const data = await prisma.movie.findMany({
+    select: {
+      id: true,
+      overview: true,
+      title: true,
+      watchLists: true,
+      imageString: true,
+      videoSorce: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    take: 4,
+  });
+  return data;
+};
+
+const RecentlyAdded = async () => {
+  const data = await getData();
+  return (
+    <div className="grid  grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-8 gap-6">
+      {data.map((movie) => (
+        <div key={movie.id} className="relative h-48">
+          <Image
+            src={movie.imageString}
+            alt="Movie"
+            className="rounded-sm absolute h-full w-full object-cover"
+            width={500}
+            height={400}
+          />
+          <div className="h-60 relative z-10 w-full transform transition duration-500 hover:scale-125 opacity-0 hover:opacity-100">
+            <Image
+              src={movie.imageString}
+              alt="Movie"
+              width={800}
+              height={800}
+              className="absolute w-full h-full -z-10 rounded-lg object-cover"
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default RecentlyAdded;
