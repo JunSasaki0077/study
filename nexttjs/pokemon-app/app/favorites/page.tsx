1e52db2080b1ca1270191415ed401c2b8be6a104
Jun Sasaki <ikasas0077@gmail.com>
2025-01-20 23:27:25 +0900

    お気に入りページ作成

--- nexttjs/pokemon-app/app/favorites/page.tsx
+++ nexttjs/pokemon-app/app/favorites/page.tsx
@@ -1,4 +1,50 @@
-const page = () => {
-  return <div>favorite</div>;
+"use client";
+
+import Header from "@/components/Header";
+import PokemonCard from "@/components/PokemonCard";
+import { useGlobalContext } from "@/context/globalContext";
+import { useEffect, useState } from "react";
+
+const Page = () => {
+  const { fetchPokemonByName, userDetails } = useGlobalContext();
+
+  const [likedPokemons, setLikedPokemons] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (userDetails?.liked) {
+      setLoading(true);
+      const fetchPokemons = async () => {
+        const pokemonDetails = await Promise.all(
+          userDetails.liked.map(async (pokemon: any) => {
+            const details = await fetchPokemonByName(pokemon);
+
+            return details;
+          })
+        );
+
+        setLikedPokemons(pokemonDetails as any);
+      };
+      setLoading(false);
+      fetchPokemons();
+    }
+  }, [userDetails?.liked]);
+
+  return (
+    <main>
+      <Header />
+      <section className="min-h-[91vh]">
+        {likedPokemons.length > 0 ? (
+          <div className="px-16 py-8 grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {likedPokemons.map((pokemon: any, index: number) => (
+              <PokemonCard key={pokemon.name + index} pokemon={pokemon} />
+            ))}
+          </div>
+        ) : (
+          <div>No liked pokemons</div>
+        )}
+      </section>
+    </main>
+  );
 };
-export default page;
+export default Page;
