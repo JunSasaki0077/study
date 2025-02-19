dafbe7036e692f74a345a9c6f6ada4b4ea9f5621
Jun Sasaki <ikasas0077@gmail.com>
2025-02-20 01:11:25 +0900

    Tabレイアウト作成

---
+++ reactnative/firstApp/app/(root)/(tabs)/_layout.tsx
@@ -0,0 +1,10 @@
+import { Tabs } from "expo-router";
+import { View, Text } from "react-native";
+const TabsLayout = () => {
+  return (
+    <Tabs screenOptions={{}}>
+      <Text>TabsLayout</Text>
+    </Tabs>
+  );
+};
+export default TabsLayout;
