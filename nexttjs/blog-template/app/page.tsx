c8bbc70f72970c6184cfa7ee8730c51e3225de7d
Jun Sasaki <ikasas0077@gmail.com>
2025-03-01 03:24:25 +0900

    完成

--- nexttjs/blog-template/app/page.tsx
+++ nexttjs/blog-template/app/page.tsx
@@ -1,17 +1,6 @@
-import process from 'node:process'
-import { type Content, createClient } from 'newt-client-js'
+import type { Post } from './_util/newt-client'
 import Link from 'next/link'
-
-const client = createClient({
-  spaceUid: 'blog-216815',
-  token: process.env.API_KEY as string,
-  apiType: 'cdn', // You can specify "cdn" or "api".
-})
-
-interface Post extends Content {
-  title: string
-  body: string
-}
+import { client } from './_util/newt-client'
 
 export default async function Page() {
   const data = await client.getContents<Post>({
@@ -20,25 +9,19 @@ export default async function Page() {
   })
 
   return (
-    <div>
-      <header>
-        <div className="bg-red-200 p-4 text-4xl font-bold text-red-900">
-          hello
-        </div>
-      </header>
-      <main>
-        <ul className="list-inside list-disc">
-          {data.items.map(item => (
-            <li key={item._id}>
-              <h1>
-                <Link href={`/post/${item._id}`} className="underline">
-                  {item.title}
-                </Link>
-              </h1>
-            </li>
-          ))}
-        </ul>
-      </main>
+    <div className="p-4">
+      <div>記事一覧</div>
+      <ul className="list-disc">
+        {data.items.map(item => (
+          <li key={item._id}>
+            <h1>
+              <Link href={`/post/${item._id}`} className="underline">
+                {item.title}
+              </Link>
+            </h1>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
---
+++ nexttjs/blog-template/app/post/[slug]/page.tsx
@@ -0,0 +1,24 @@
+import { client, type Post } from '~/app/_util/newt-client'
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>
+}) {
+  const { slug } = await params
+  const data = await client.getContent<Post>({
+    appUid: 'blog',
+    modelUid: 'post',
+    contentId: slug,
+  })
+  return (
+    <div className="p-4">
+      <h1>{data.title}</h1>
+      {/* eslint-disable-next-line react-dom/no-dangerously-set-innerhtml */}
+      <article
+        className="prose prose-sm"
+        dangerouslySetInnerHTML={{ __html: data.body }}
+      />
+    </div>
+  )
+}
