e64728601f30655fbbac994f61b5443992f026d3
Jun Sasaki <ikasas0077@gmail.com>
2025-07-16 05:36:50 +0900

    signup,signinページ作成

---
+++ reactnative/fast_food/app/(auth)/_layout.tsx
@@ -0,0 +1,11 @@
+import { Slot } from 'expo-router'
+import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+export default function _Layout() {
+  return (
+    <SafeAreaView>
+      <Text>auth layout</Text>
+      <Slot />
+    </SafeAreaView>
+  )
+}
