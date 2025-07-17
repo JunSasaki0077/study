75db4f0462e5f040696412f23a9e378302afe465
Jun Sasaki <ikasas0077@gmail.com>
2025-07-18 07:10:03 +0900

    signup,signinページ見た目作成

---
+++ reactnative/fast_food/lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+	endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+	projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+	platform: "com.jsm.foodordering",
+	databaseId: "6876c3990008e6e74201",
+	userCollectionId: "6876c45f00297e8ea7dc"
+}
